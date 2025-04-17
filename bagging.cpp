c8c94a721adee82c40f0a6b1a0b029b4019f5bff
Mike <fysh4202@163.com>
2025-04-17 14:09:35 +0800

    bestfeature

--- bagging.cpp
+++ bagging.cpp
@@ -7,14 +7,14 @@
 
 #include "bagging.hpp"
 
-void randomSampling(void* src_feature, void * src_label, void ** &dst_feature, void ** &dst_label, 
-    unsigned src_size, unsigned src_dim, unsigned dst_size, unsigned dst_dim, unsigned dst_count, 
-    unsigned feature_bytes, unsigned label_bytes) {
+void randomSampling(void* src_feature, void* src_label, void**& dst_feature, void**& dst_label,
+  unsigned src_size, unsigned src_dim, unsigned dst_size, unsigned dst_dim, unsigned dst_count,
+  unsigned feature_bytes, unsigned label_bytes) {
   srand(time(NULL));
-  dst_feature = (void**)malloc(dst_count * sizeof(void *));
-  dst_label = (void**)malloc(dst_count * sizeof(void *));
-  unsigned *indexx = (unsigned*)malloc(dst_size * sizeof(int));
-  unsigned *indexy = (unsigned*)malloc(dst_dim * sizeof(int));
+  dst_feature = (void**)malloc(dst_count * sizeof(void*));
+  dst_label = (void**)malloc(dst_count * sizeof(void*));
+  unsigned* indexx = (unsigned*)malloc(dst_size * sizeof(int));
+  unsigned* indexy = (unsigned*)malloc(dst_dim * sizeof(int));
   for (unsigned i = 0; i < dst_count; ++i) {
     dst_feature[i] = malloc(dst_size * dst_dim * feature_bytes);
     dst_label[i] = malloc(dst_size * label_bytes);
@@ -23,10 +23,10 @@ void randomSampling(void* src_feature, void * src_label, void ** &dst_feature, v
     for (unsigned j = 0, J = 0; j < dst_size; ++j, J += dst_dim) {
       memcpy((char*)dst_label[i] + j * label_bytes,
         (char*)src_label + indexx[j] * label_bytes, label_bytes);
-        indexx[j] *= src_dim;
+      indexx[j] *= src_dim;
       for (unsigned k = 0, K = J; k < dst_dim; ++k, ++K) {
-        memcpy((char*)dst_feature[i] + K * feature_bytes, 
-          (char*)src_feature + (indexx[j] + indexy[k]) * feature_bytes, 
+        memcpy((char*)dst_feature[i] + K * feature_bytes,
+          (char*)src_feature + (indexx[j] + indexy[k]) * feature_bytes,
           feature_bytes);
       }
     }
@@ -35,13 +35,13 @@ void randomSampling(void* src_feature, void * src_label, void ** &dst_feature, v
   free(indexy);
 }
 
-void randomSampling4Bytes(void* src_feature, void * src_label, void ** &dst_feature, void ** &dst_label, 
+void randomSampling4Bytes(void* src_feature, void* src_label, void**& dst_feature, void**& dst_label,
   unsigned src_size, unsigned src_dim, unsigned dst_size, unsigned dst_dim, unsigned dst_count) {
   srand(time(NULL));
   dst_feature = (void**)malloc(dst_count * sizeof(void*));
   dst_label = (void**)malloc(dst_count * sizeof(void*));
-  unsigned *indexx = (unsigned*)malloc(dst_size * sizeof(int));
-  unsigned *indexy = (unsigned*)malloc(dst_dim * sizeof(int));
+  unsigned* indexx = (unsigned*)malloc(dst_size * sizeof(int));
+  unsigned* indexy = (unsigned*)malloc(dst_dim * sizeof(int));
   for (unsigned i = 0; i < dst_count; ++i) {
     dst_feature[i] = malloc(dst_size * dst_dim * 4);
     dst_label[i] = malloc(dst_size * 4);
@@ -66,10 +66,10 @@ signed main(int argc, char* argv[]) {
   unsigned dst_count = 10;
   unsigned feature_bytes = sizeof(float);
   unsigned label_bytes = sizeof(int);
-  
+
   float* src_feature = (float*)malloc(src_size * src_dim * feature_bytes);
   int* src_label = (int*)malloc(src_size * label_bytes);
-  
+
   for (unsigned i = 0; i < src_size; ++i) {
     for (unsigned j = 0; j < src_dim; ++j) {
       src_feature[i * src_dim + j] = static_cast<float>(rand()) / RAND_MAX;
@@ -78,30 +78,30 @@ signed main(int argc, char* argv[]) {
   }
   void** dst_feature;
   void** dst_label;
-  
+
   unsigned long long TimeBegin = clock(), TimeEnd;
-  randomSampling(src_feature, src_label, dst_feature, dst_label, 
-    src_size, src_dim, dst_size, dst_dim, 
+  randomSampling(src_feature, src_label, dst_feature, dst_label,
+    src_size, src_dim, dst_size, dst_dim,
     dst_count, feature_bytes, label_bytes);
   TimeEnd = clock();
   printf("Time %llu us\n", TimeEnd - TimeBegin);
   TimeBegin = clock();
-  randomSampling4Bytes(src_feature, src_label, dst_feature, dst_label, 
+  randomSampling4Bytes(src_feature, src_label, dst_feature, dst_label,
     src_size, src_dim, dst_size, dst_dim, dst_count);
   TimeEnd = clock();
   printf("Time %llu us\n", TimeEnd - TimeBegin);
-  
+
   // Free allocated memory
   free(src_feature);
   free(src_label);
-  
+
   for (unsigned i = 0; i < dst_count; ++i) {
     free(dst_feature[i]);
     free(dst_label[i]);
   }
-  
+
   free(dst_feature);
   free(dst_label);
-  
+
   return 0;
 }
--- metrics.cpp
+++ metrics.cpp
@@ -5,19 +5,19 @@
 #include <algorithm>
 
 const unsigned dataSize = 1000000;
-const unsigned dataDim = 1;
+const unsigned dataDim = 10;
 const unsigned classCount = 10;
 
 void computeHistogram(unsigned* label, unsigned*& hist, unsigned sample_count, unsigned class_count) {
   hist = (unsigned*)malloc(class_count * sizeof(unsigned));
   memset(hist, 0, class_count * sizeof(unsigned));
-  
+
   for (unsigned i = 0; i < sample_count; i++) ++hist[label[i]];
 }
 float computeEntropyWithHist(unsigned* hist, unsigned sample_count, unsigned class_count) {
-  if(sample_count == 0) return 0.0f;
+  if (sample_count == 0) return 0.0f;
   float entropy = 0.0f;
-  
+
   for (unsigned i = 0; i < class_count; i++) {
     float probab = (float)hist[i] / sample_count;
     entropy -= probab * log2(probab);
@@ -25,7 +25,7 @@ float computeEntropyWithHist(unsigned* hist, unsigned sample_count, unsigned cla
   return entropy;
 }
 float computeEntropy(unsigned* label, unsigned sample_count, unsigned class_count) {
-  if(sample_count == 0) return 0.0f;
+  if (sample_count == 0) return 0.0f;
   unsigned* hist;
   computeHistogram(label, hist, sample_count, class_count);
   float entropy = computeEntropyWithHist(hist, sample_count, class_count);
@@ -34,54 +34,76 @@ float computeEntropy(unsigned* label, unsigned sample_count, unsigned class_coun
 }
 
 std::pair<float, float> informationGain(float* feature, unsigned* label, unsigned sample_count, unsigned class_count) {
-  if(sample_count == 0) return {0.0f, 0.0f};
+  if (sample_count == 0) return { 0.0f, 0.0f };
 
   float entropy, entropy_y, splitPoint;
   float entropy_l, entropy_r;
-  std::pair<float, unsigned> *f_l = (std::pair<float, unsigned> *)malloc(sample_count * sizeof(std::pair<float, unsigned>));
-  unsigned *histl = (unsigned *)malloc(class_count * sizeof(unsigned)), *histr;
+  std::pair<float, unsigned>* f_l = (std::pair<float, unsigned> *)malloc(sample_count * sizeof(std::pair<float, unsigned>));
+  unsigned* histl = (unsigned*)malloc(class_count * sizeof(unsigned)), * histr;
   memset(histl, 0, class_count * sizeof(unsigned));
   computeHistogram(label, histr, sample_count, class_count);
 
-  for (unsigned i = 0; i < sample_count; ++i) f_l[i] = {feature[i], label[i]};
+  for (unsigned i = 0; i < sample_count; ++i) f_l[i] = { feature[i], label[i] };
   std::sort(f_l, f_l + sample_count);
 
   entropy_r = entropy = computeEntropyWithHist(histr, sample_count, class_count);
   entropy_l = 0;
   entropy_y = entropy_l + entropy_r;
   splitPoint = f_l[0].first - 0.001f;
-  
+
   for (unsigned i = 0; i < sample_count - 1; ++i) {
     histl[f_l[i].second]++;
     histr[f_l[i].second]--;
-    if(f_l[i].first == f_l[i + 1].first) continue;
+    if (f_l[i].first == f_l[i + 1].first) continue;
     entropy_l = ((i + 1.0f) / sample_count) * computeEntropyWithHist(histl, i + 1, class_count);
     entropy_r = ((sample_count - i - 1.0f) / sample_count) * computeEntropyWithHist(histr, sample_count - i - 1, class_count);
-    if(entropy_l + entropy_r < entropy_y) {
+    if (entropy_l + entropy_r < entropy_y) {
       entropy_y = entropy_l + entropy_r;
       splitPoint = (f_l[i].first + f_l[i + 1].first) / 2.0f;
     }
   }
-  return {entropy - entropy_y, splitPoint};
+  return { entropy - entropy_y, splitPoint };
+}
+std::pair<unsigned, float> findBestFeature(float* feature, unsigned* label,
+  unsigned sample_count, unsigned dataDim, unsigned class_count) {
+  if (sample_count == 0) return { 0, 0 };
+
+  float maxIG = -0.01f;
+  float splitPoint = 0.0f;
+  unsigned bestFeature = 0;
+  float* featureColumn = (float*)malloc(sample_count * sizeof(float));
+
+  for (unsigned i = 0; i < dataDim; ++i) {
+    for (unsigned j = 0, J = i; j < sample_count; ++j, J += dataDim)
+      featureColumn[j] = feature[J];
+    std::pair<float, float> IGp = informationGain(featureColumn, label, sample_count, class_count);
+    if (IGp.first > maxIG) {
+      maxIG = IGp.first;
+      splitPoint = IGp.second;
+      bestFeature = i;
+    }
+  }
+  return { maxIG, splitPoint };
 }
 
 signed main() {
-  float *h_feature = (float *)malloc(dataSize * dataDim * sizeof(float));
-  unsigned *h_label = (unsigned *)malloc(dataSize * sizeof(unsigned));
-  
+  float* h_feature = (float*)malloc(dataSize * dataDim * sizeof(float));
+  unsigned* h_label = (unsigned*)malloc(dataSize * sizeof(unsigned));
+
   printf("Done");
   // 生成随机数据 (0-1之间)
-  for (unsigned i = 0, I = 0; i < dataSize; ++i, I += dataDim) 
-    for (unsigned j = 0, J = I; j < dataDim; ++j, ++J) 
+  for (unsigned i = 0, I = 0; i < dataSize; ++i, I += dataDim)
+    for (unsigned j = 0, J = I; j < dataDim; ++j, ++J)
       h_feature[J] = (float)rand() / RAND_MAX;
   for (unsigned i = 0; i < dataSize; i++) h_label[i] = rand() % classCount;
-  
+
   // 计算熵
   unsigned long long TimeBegin = clock(), TimeEnd;
-  std::pair<float, float> IGp = informationGain(h_feature, h_label, dataSize, classCount);
+  std::pair<unsigned, float> BestFeature = findBestFeature(h_feature, h_label,
+    dataSize, dataDim, classCount);
   TimeEnd = clock();
-  printf("IG: %f Split %f Time %llu us\n", IGp.first, IGp.second, TimeEnd - TimeBegin);
-  
+  printf("BestFeature: %u Split %f Time %llu us\n", BestFeature.first, BestFeature.second, TimeEnd - TimeBegin);
+
 
   return 0;
 }
